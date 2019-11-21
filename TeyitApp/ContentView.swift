ba2d15effb1a3dfdbe47162942c0e3059013aca7
orcunozyurt <orcunozyurt@hotmail.com>
2019-11-21 21:49:46 +0100

    Initial Commit

---
+++ TeyitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeyitApp
+//
+//  Created by Orcun Ozyurt on 11/21/19.
+//  Copyright © 2019 Orcun Ozyurt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
